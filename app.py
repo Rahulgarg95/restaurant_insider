e01f8e8b6b230be8154706f4e479d920031aaf2c
GARG <rahulgarg95@gmail.com>
2018-09-05 20:35:55 +0530

    Bootstrap

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 from flask import Flask
+from flask import render_template
+from flask import Flask, Response, redirect, url_for, request, session, abort
+from flask_login import LoginManager, UserMixin, \
+                                login_required, login_user, logout_user
 
 app = Flask(__name__)
+login_manager = LoginManager(app)
 
 @app.route("/")
 def home():
-   return "Its going to be fun"
+   return render_template("home.html")
+
+@app.route("/account")
+@login_required
+def account():
+   return "You are logged in"
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
