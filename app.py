4613ba0d6d88b387ec6e53647a986b22c3e95cb4
GARG <rahulgarg95@gmail.com>
2018-09-06 22:11:54 +0530

    db connection

--- app.py
+++ app.py
@@ -3,9 +3,21 @@ from flask import render_template
 from flask import Flask, Response, redirect, url_for, request, session, abort
 from flask_login import LoginManager, UserMixin, \
                                 login_required, login_user, logout_user
+from flask.ext.login import login_user
+from mockdbhelper import MockDBHelper as DBHelper
+from user import User
+from flask import redirect
+from flask import url_for
+from flask import request
+
+DB = DBHelper()
 
 app = Flask(__name__)
+
+app.secret_key = 'tPXJY3X37Qybz4QykV+hOyUxVQeEXf1Ao2C8upz+fGQXKsM'
+
 login_manager = LoginManager(app)
+
 
 @app.route("/")
 def home():
@@ -15,6 +27,17 @@ def home():
 @login_required
 def account():
    return "You are logged in"
+
+@app.route("/login", methods=["POST"])
+def login():
+   email = request.form.get("email")
+   password = request.form.get("password")
+   user_password = DB.get_user(email)
+   if user_password and user_password == password:
+      user = User(email)
+      login_user(user)
+      return redirect(url_for('account'))
+   return home()
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
